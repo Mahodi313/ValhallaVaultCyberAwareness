98a92bf71b4a9a60e13853602ccd36bedb08bb68
EmmaDQ <sydemma@hotmail.com>
2024-03-04 14:04:26 +0100

    add dbModels

---
+++ Shared/DbModels/QuestionModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DbModels
+{
+    public class QuestionModel
+    {
+        [Key]
+        int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public string CorrectAnswer { get; set; } = null!;
+        public int SubcategoryId { get; set; }
+        public SubcategoryModel Subcategory { get; set; } = null!;
+        public List<UserResponseModel> UserResponse { get; set; } = new();
+
+    }
+}
---
+++ Shared/DbModels/SegmentModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DbModels
+{
+    public class SegmentModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int CategoryId { get; set; }
+        public CategoryModel Category { get; set; } = null!;
+        public List<SubcategoryModel> Subcategorys { get; set; } = new();
+    }
+}
