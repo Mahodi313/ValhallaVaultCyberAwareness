798458244e6fe6fe4e8a120e5775c8bddd657484
Mahodi313 <MehdiAli15@outlook.com>
2024-03-05 20:52:23 +0100

    SeedDbUpdate

--- Shared/DbModels/AnswerModel.cs
+++ Shared/DbModels/AnswerModel.cs
@@ -8,7 +8,7 @@ namespace Shared.DbModels
         public int Id { get; set; }
         public string Answer { get; set; } = null!;
         public bool IsCorrectAnswer { get; set; }
-        public string Explanation { get; set; }
+        public string? Explanation { get; set; }
         public int QuestionId { get; set; }
         public QuestionModel Question { get; set; } = null!;
     }
