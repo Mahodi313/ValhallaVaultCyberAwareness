793f4bd7f2afee42717c2af8293ee2571bb0e4d2
Adirsflo <rida.abdal@gmail.com>
2024-03-06 10:59:56 +0100

    Repository for Question & Answer

---
+++ Shared/Repository/Interfaces/IQuestionRepository.cs
@@ -0,0 +1,11 @@
+﻿using ValhallaVaultCyberAwareness.DAL.DbModels;
+
+namespace ValhallaVaultCyberAwareness.DAL.Repository.Interfaces
+{
+    public interface IQuestionRepository : IModelRepository<QuestionModel>
+    {
+        Task<IEnumerable<QuestionModel>> GetQuestionsBySubcategoryAsync(int subcategoryId);
+        Task<IEnumerable<AnswerModel>> GetAnswerForQuestionAsync(int questionId);
+        Task<IEnumerable<UserResponseModel>> GetUserResponsesForQuestionAsync(int questionId);
+    }
+}
