c4c9fae5df5a59e51b0039734a728be9d2e5f12d
Adirsflo <rida.abdal@gmail.com>
2024-03-07 13:36:33 +0100

    Handeling conflict + QuestionPage

--- Shared/Repository/IRepository.cs
+++ Shared/Repository/IRepository.cs
@@ -7,21 +7,21 @@ namespace ValhallaVaultCyberAwareness.DAL.Repository
         // General CRUD-funktion for all Repositories
         public Task<T> CreateAsync(T entity);
         public Task<T> DeleteAsync(int id);
-        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<List<T>> GetAllAsync();
         public Task<T> GetByIdAsync(int id);
         public Task UpdateAsync(T entity);
 
         // QuestionRepository
-        Task<IEnumerable<QuestionModel>> GetQuestionsBySubcategoryAsync(int subcategoryId);
-        Task<IEnumerable<AnswerModel>> GetAnswerForQuestionAsync(int questionId);
-        Task<IEnumerable<UserResponseModel>> GetUserResponsesForQuestionAsync(int questionId);
+        Task<List<QuestionModel>> GetQuestionsBySubcategoryAsync(int subcategoryId);
+        Task<List<AnswerModel>> GetAnswerForQuestionAsync(int questionId);
+        Task<List<UserResponseModel>> GetUserResponsesForQuestionAsync(int questionId);
 
         // AnswerRepository
-        Task<IEnumerable<AnswerModel>> GetByQuestionIdAsync(int questionId);
-        Task<IEnumerable<AnswerModel>> GetCorrectAnswersByQuestionIdAsync(int questionId);
+        Task<List<AnswerModel>> GetByQuestionIdAsync(int questionId);
+        Task<List<AnswerModel>> GetCorrectAnswersByQuestionIdAsync(int questionId);
 
         // SubcategoryRepository
-        Task<IEnumerable<SubcategoryModel>> GetSubcategoryBySegmentAsync(int segmentId);
-        Task<IEnumerable<QuestionModel>> GetQuestionForSubcategoryAsync(int subcategoryId);
+        Task<List<SubcategoryModel>> GetSubcategoryBySegmentAsync(int segmentId);
+        Task<List<QuestionModel>> GetQuestionForSubcategoryAsync(int subcategoryId);
     }
 }
