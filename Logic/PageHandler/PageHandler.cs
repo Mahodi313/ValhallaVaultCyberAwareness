3afa43af5f9caa2e15cfe141b43cfce070a41c17
EmmaDQ <sydemma@hotmail.com>
2024-03-14 16:37:13 +0100

    add navbar component to category, segment and subcategory pages

---
+++ Logic/PageHandler/PageHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace ValhallaVaultCyberAwareness.App.PageHandler
+{
+    public static class PageHandler
+    {
+        public static int? CategoryId { get; set; }
+        public static bool firstCategory { get; set; }
+        public static int? SegmentId { get; set; }
+        public static int? SubcategoryId { get; set; }
+
+        public static int? SubcategoryIdHoledr { get; set; }
+        public static bool whentToSubcategory { get; set; }
+
+    }
+}
