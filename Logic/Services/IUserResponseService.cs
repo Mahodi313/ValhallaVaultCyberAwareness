e460d63d4658f105cdd8cd16c21a038425d911f7
Mahodi313 <MehdiAli15@outlook.com>
2024-03-15 02:09:11 +0100

    FixedCalculationError

--- Logic/Services/IUserResponseService.cs
+++ Logic/Services/IUserResponseService.cs
@@ -6,7 +6,7 @@ namespace ValhallaVaultCyberAwareness.App.Services
     public interface IUserResponseService
     {
         public Task<List<UserResponseModel>> GetUserResponsesAsync(string userId);
-        public int CalculateSegmentCompletionBasedOnUser(SegmentModel segment, List<UserResponseModel> userResponses);
-        public int CalculateSegmentCompletionBasedOnUser(SegmentViewModel segment, List<UserResponseModel> userResponses);
+        public Task<int> CalculateSegmentCompletionBasedOnUser(SegmentModel segment, List<UserResponseModel> userResponses);
+        public Task<int> CalculateSegmentCompletionBasedOnUser(SegmentViewModel segment, List<UserResponseModel> userResponses);
     }
 }
